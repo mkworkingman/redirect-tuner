9cd3a18b38f4db1f101e67726de157c6f8235bb1
Maxim Kalinin <maxim.kalinin@libertexgroup.com>
2022-04-09 21:12:41 +0200

    [master] Add styles

--- components/Tuner.vue
+++ components/Tuner.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
     <h2>Tuner:</h2>
-    <audio :src="this.audioUrl" ref="track" controls></audio>
     <h3>Frequency: {{this.frequency}} Hz</h3>
+    <h3 v-show="!frequency">Please, play louder.</h3>
   </div>
 </template>
 
 <script>
-import aubio from "aubiojs";
-import andante from '@/assets/andante.mp3'
-import freq440hz from '@/assets/440hz.mp3'
-
 export default {
   props: {
     fileName: String
@@ -33,16 +29,16 @@ export default {
       listening: true,
       keysSharp: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
       keysFlat: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'],
-      frequency: null,
+      frequency: null
     }
   },
   methods: {
     async setup() {
       this.audioContext = new AudioContext()
       this.analyser = new AnalyserNode(this.audioContext, { fftSize: this.fftSize })
-      this.analyser.minDecibels = -100;
-      this.analyser.maxDecibels = -10;
-      this.analyser.smoothingTimeConstant = 0.85;
+      this.analyser.minDecibels = -100
+      this.analyser.maxDecibels = -10
+      this.analyser.smoothingTimeConstant = 0.85
       this.mic = await navigator.mediaDevices.getUserMedia({
         audio: true
       })
@@ -60,91 +56,93 @@ export default {
     test() {
       requestAnimationFrame(this.test)
       const buffer = new Float32Array(this.fftSize) // this.dataArray
-      this.analyser.getFloatTimeDomainData(buffer)
+      this.analyser.getFloatTimeDomainData(this.dataArray)
 
-      this.frequency = this.detectFrequency(buffer, this.audioContext.sampleRate).toFixed(1)
+      // this.frequency = this.detectFrequency()
+      this.frequency = this.detectFrequency() || this.frequency
     },
-    detectFrequency(buffer, sampleRate) {
+    detectFrequency() {
       // Perform a quick root-mean-square to see if we have enough signal
-      var SIZE = buffer.length;
-      var sumOfSquares = 0;
-      for (var i = 0; i < SIZE; i++) {
-        var val = buffer[i];
-        sumOfSquares += val * val;
+      let buffer = this.dataArray
+      let size = buffer.length
+      let sumOfSquares = 0
+      for (let i = 0; i < size; i++) {
+        let val = buffer[i]
+        sumOfSquares += val * val
       }
-      var rootMeanSquare = Math.sqrt(sumOfSquares / SIZE)
+      const rootMeanSquare = Math.sqrt(sumOfSquares / size)
       if (rootMeanSquare < 0.01) {
-        return -1;
+        return 0
       }
 
       // Find a range in the buffer where the values are below a given threshold.
-      var r1 = 0;
-      var r2 = SIZE - 1;
-      var threshold = 0.2;
+      let r1 = 0
+      let r2 = size - 1
+      const threshold = 0.2
 
       // Walk up for r1
-      for (var i = 0; i < SIZE / 2; i++) {
+      for (let i = 0; i < size / 2; i++) {
         if (Math.abs(buffer[i]) < threshold) {
-          r1 = i;
-          break;
+          r1 = i
+          break
         }
       }
 
       // Walk down for r2
-      for (var i = 1; i < SIZE / 2; i++) {
-        if (Math.abs(buffer[SIZE - i]) < threshold) {
-          r2 = SIZE - i;
-          break;
+      for (let i = 1; i < size / 2; i++) {
+        if (Math.abs(buffer[size - i]) < threshold) {
+          r2 = size - i
+          break
         }
       }
 
-      // Trim the buffer to these ranges and update SIZE.
-      buffer = buffer.slice(r1, r2);
-      SIZE = buffer.length
+      // Trim the buffer to these ranges and update size.
+      buffer = buffer.slice(r1, r2)
+      size = buffer.length
 
       // Create a new array of the sums of offsets to do the autocorrelation
-      var c = new Array(SIZE).fill(0);
+      const c = new Array(size).fill(0)
       // For each potential offset, calculate the sum of each buffer value times its offset value
-      for (let i = 0; i < SIZE; i++) {
-        for (let j = 0; j < SIZE - i; j++) {
+      for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size - i; j++) {
           c[i] = c[i] + buffer[j] * buffer[j+i]
         }
       }
 
       // Find the last index where that value is greater than the next one (the dip)
-      var d = 0;
+      let d = 0
       while (c[d] > c[d+1]) {
-        d++;
+        d++
       }
 
       // Iterate from that index through the end and find the maximum sum
-      var maxValue = -1;
-      var maxIndex = -1;
-      for (var i = d; i < SIZE; i++) {
+      let maxValue = -1
+      let maxIndex = -1
+      for (let i = d; i < size; i++) {
         if (c[i] > maxValue) {
-          maxValue = c[i];
-          maxIndex = i;
+          maxValue = c[i]
+          maxIndex = i
         }
       }
 
-      var T0 = maxIndex;
+      let T0 = maxIndex
 
       // Not as sure about this part, don't @ me
       // From the original author:
       // interpolation is parabolic interpolation. It helps with precision. We suppose that a parabola pass through the
       // three points that comprise the peak. 'a' and 'b' are the unknowns from the linear equation system and b/(2a) is
       // the "error" in the abscissa. Well x1,x2,x3 should be y1,y2,y3 because they are the ordinates.
-      var x1 = c[T0 - 1];
-      var x2 = c[T0];
-      var x3 = c[T0 + 1]
+      const x1 = c[T0 - 1]
+      const x2 = c[T0]
+      const x3 = c[T0 + 1]
 
-      var a = (x1 + x3 - 2 * x2) / 2;
-      var b = (x3 - x1) / 2
+      const a = (x1 + x3 - 2 * x2) / 2
+      const b = (x3 - x1) / 2
       if (a) {
-        T0 = T0 - b / (2 * a);
+        T0 = T0 - b / (2 * a)
       }
 
-      return sampleRate/T0;
+      return (this.audioContext.sampleRate/T0).toFixed(1)
     }
   },
   async mounted() {
